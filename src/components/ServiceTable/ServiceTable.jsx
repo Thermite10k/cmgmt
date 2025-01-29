654a2126fd5614e83b5650bfb0a8dbd03a0105da
Ali batashi <batashi.ali@gmail.com>
2025-01-29 22:26:13 +0330

    Improved Styles

--- src/components/ServiceTable/ServiceTable.jsx
+++ src/components/ServiceTable/ServiceTable.jsx
@@ -4,15 +4,24 @@ import classes from "./ServiceTable.module.css";
 
 const ServiceTable = ({ service }) => {
   return (
-    <div className={classes.serviceContainer}>
-      <h1 className={classes.serviceTitle}>{service.key}</h1>
-      <div className={classes.timerContainer}>
-        {Array.from({ length: service.count }).map((_, index) => (
-          <Timer
-            title={service.name + " #" + (index + 1)}
-            rate={service.hourlyRate}
-          />
-        ))}
+    <div className={classes.relativeContainer}>
+      <div
+        style={{ backgroundImage: `url(${console})` }}
+        className={`${
+          service.key === "Tables" ? classes.table : classes.console
+        } ${classes.backgroundEffect} `}
+      />
+
+      <div className={classes.serviceContainer}>
+        <h1 className={classes.serviceTitle}>{service.key}</h1>
+        <div className={classes.timerContainer}>
+          {Array.from({ length: service.count }).map((_, index) => (
+            <Timer
+              title={service.name + " #" + (index + 1)}
+              rate={service.hourlyRate}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
