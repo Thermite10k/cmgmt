c15a7ba63467c19225ec62c6b864f1e5fb53cffb
Ali batashi <batashi.ali@gmail.com>
2025-02-14 13:48:18 +0330

    Added a test commit

--- src/utils/roundRials.js
+++ src/utils/roundRials.js
@@ -2,7 +2,7 @@ const roundRials = (val) => {
   let rem = 0;
 
   const minChange = 10000; // to geth the last 5 digits
-  const step = 4999; // minimum price step
+  const step = 4999; // minimum price step // test
 
   rem = Math.floor(+val / minChange);
 
