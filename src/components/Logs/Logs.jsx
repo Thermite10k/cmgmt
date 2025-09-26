370b1c8fffe89d00b469b732190dca42a08739b0
Ali batashi <batashi.ali@gmail.com>
2025-09-27 01:57:39 +0330

    Logs are visible without authentication

--- src/components/Logs/Logs.jsx
+++ src/components/Logs/Logs.jsx
@@ -52,9 +52,9 @@ const Logs = () => {
           <SingleExtra />
         </div>
       </div>
+      <Stats />
       {isAuthenticated ? (
         <div>
-          <Stats />
           <HistoryTable />
         </div>
       ) : (
